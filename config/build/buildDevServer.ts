621087098d0689828dff8f3a1c11585fa05ab3e1
TamaraMironova <mirtomma@gmail.com>
2024-07-20 16:08:38 +0300

    initial commit p2

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,23 @@
+// import { BuildOptions } from "./types/config";
+
+// export function buildDevServer(options: BuildOptions): DevServerConfiguration {
+//     return {
+//         port: options.port,
+//         open: true
+//     }
+// } так было по видосу, но у меня почему-то не работало и ругалось на devserverconfiguration
+
+import * as webpack from 'webpack';
+import * as webpackDevServer from 'webpack-dev-server';
+import { BuildOptions } from "./types/config";
+
+type Configuration = webpack.Configuration;
+type DevServerConfiguration = webpackDevServer.Configuration;
+
+export function buildDevServer(options: BuildOptions): DevServerConfiguration {
+    return {
+        port: options.port,
+        open: true,
+        historyApiFallback: true,
+    };
+}
